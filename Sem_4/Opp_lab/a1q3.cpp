eb2b311c92b79a81d75c210b43b36543eabd5e58
alphaLab <alpha@lab.com>
2019-01-28 13:46:15 +0530

    a1q3 uncomplete 3

--- Sem_4/Opp_lab/a1q3.cpp
+++ Sem_4/Opp_lab/a1q3.cpp
@@ -1,9 +1,18 @@
 #include<iostream>
-
+#include<string>
 using namespace std;
+
+
+class item
+{
+    int code;
+    string name;
+    int quality;
+    float rate;
+}
 
 int main()
 {
-    
+
     return 0;
 }
