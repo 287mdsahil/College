ffccb0a5395a1da667a0732314f3aed40f589b2f
Md Sahil <287mdsahil@gmail.com>
2019-02-03 12:38:32 +0530

    a1q3 uncomplete 4

--- Sem_4/Opp_lab/a1q3.cpp
+++ Sem_4/Opp_lab/a1q3.cpp
@@ -5,14 +5,70 @@ using namespace std;
 
 class item
 {
-    int code;
+    string code;
     string name;
-    int quality;
+    int quantity;
     float rate;
+    
+    public:
+    
+    void getItem()
+    {
+        cout<<"Enter the code: ";
+        cin>>code;
+        cout<<"Enter the name: ";
+        cin>>name;
+        cout<<"Enter the quantity: ";
+        cin>>quantity;
+        cout<<"Enter the rate: ";
+        cin>>rate;
+        return;
+    }
+
+    void showItem()
+    {
+        cout<<"Item code:\t"<<code<<endl;
+        cout<<"Item name:\t"<<name<<endl;
+        cout<<"Item quantity:\t"<quantity<<endl;
+        cout<<"Item rate:\t"<<rate<<endl;
+        return;
+    }
+
+    void setPrice(float p)
+    {
+        rate = p;
+        return;
+    }
+
+    int setQuantity(int q)
+    {
+        quantity = p;
+        return;
+    }
+
+    string getCode()
+    {
+        return code;
+    }
+
+    string getName()
+    {
+        return name;
+    }
+
+    float getRate()
+    {
+        return rate;
+    }
+
+    int getQuantity()
+    {
+        return quantity;
+    }
+
 }
 
 int main()
 {
-
     return 0;
 }
