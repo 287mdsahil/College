d8d37d25a2cffc36392de9508f144fb858c3f90a
Md Sahil <287mdsahil@gmail.com>
2019-02-16 09:05:54 +0530

    a1q4 implement setData showData of student

--- Sem_4/Opp_lab/a1q4.cpp
+++ Sem_4/Opp_lab/a1q4.cpp
@@ -1,27 +1,41 @@
 #include<iostream>
+#include<utility>
+#include<string.h>
 using namespace std;
 
 class student{
     int roll;
     char name[31];
-    char course[31];
+    char course[51];
     char admission_date[11];
-    int marks[5];
+    pair<char[31],int> marks[5];
 
     public:
+
+    student(){
+        strncpy(marks[0].first,"Physics",31);
+        strncpy(marks[1].first,"Maths",31);
+        strncpy(marks[2].first,"Chemestry",31);
+        strncpy(marks[3].first,"English",31);
+        strncpy(marks[4].first,"P.Ed",31);
+    }
 
     void setData(){
         cout<<"Enter roll no.: ";
         cin>>roll;
+        cin.ignore(100,'\n');
         cout<<"Enter the name: ";
-        cin>>name;
+        cin.getline(name,31);
         cout<<"Enter the course: ";
-        cin>>course;
+        cin.getline(course,51);
         cout<<"Enter the admission date: ";
         cin>>admission_date;
-        cout<<"Enter the marks of the 5 subjects: ";
-        for(int i=0;i<5;i++) 
-            cin>>marks[i];
+        cin.ignore(100,'\n');
+        for(int i=0;i<5;i++){
+            cout<<"Enter marks in "<<marks[i].first<<": "; 
+            cin>>marks[i].second;
+        }
+        cin.ignore(100,'\n');
         return;
     }
 
@@ -30,10 +44,8 @@ class student{
         cout<<"Name: "<<name<<endl;
         cout<<"Course: "<<course<<endl;
         cout<<"Admission date: "<<admission_date<<endl;
-        cout<<"Marks:";
         for(int i=0;i<5;i++)
-            cout<<" "<<marks[i];
-        cout<<endl;
+            cout<<"Marks in "<<marks[i].first<<": "<<marks[i].second<<endl;
         return;
     }
 };
@@ -43,5 +55,8 @@ int main(){
     student s;
     s.setData();
     s.showData();
-    return 0;
+    //char s[32];
+    //cin.getline(s,32);
+    //cout<<s;
+    //return 0;
 }
