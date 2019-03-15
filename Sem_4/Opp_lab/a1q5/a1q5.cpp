0343b1f86be9aa964958d2972cffb82bedf7a177
Md Sahil <287mdsahil@gmail.com>
2019-03-15 22:36:44 +0530

    a1q5 complete

--- Sem_4/Opp_lab/a1q5/a1q5.cpp
+++ Sem_4/Opp_lab/a1q5/a1q5.cpp
@@ -137,21 +137,125 @@ Array<T> operator*(T const c,Array<T> const &a){
 
 
 int main(){
+    while(1){
+        system("clear");
+        cout<<"MENU:"<<endl;
+        cout<<"1.Add two objects"<<endl;
+        cout<<"2.Assign one object by another"<<endl;
+        cout<<"3.Iterating through Array elements"<<endl;
+        cout<<"4.Multiplication with constant"<<endl;
+        cout<<"5.Exit"<<endl;
+        cout<<"Enter choice: ";
+        int choice;
+        cin>>choice;
+        cin.ignore();
+        
+        switch (choice)
+        {
+            case 1:{
+                int n1,n2;
+                cout<<"Enter the size of first array: ";
+                cin>>n1;
+                cin.ignore();
+                cout<<"Enter the elements: ";
+                int ar1[n1];
+                for(int i=0;i<n1;i++)
+                    cin>>ar1[i];
+                cin.ignore();
+                cout<<"Enter the size of second array: ";
+                cin>>n2;
+                cin.ignore();
+                cout<<"Enter the elements: ";
+                int ar2[n2];
+                for(int i=0;i<n2;i++)
+                    cin>>ar2[i];
+                cin.ignore();
+                Array<int> a1(ar1,n1);
+                Array<int> a2(ar2,n2);
+                Array<int> sum = a1+a2;
+                cout<<endl<<"Resultant array: ";
+                for(int i=0;i<sum.size();i++)
+                    cout<<sum[i]<<" ";
+                cout<<endl;
+                getchar();
+                break;
+            }
 
-    Array<int> a(5);
-    Array<int> b(a);
-    int c[]={1,2,3};
-    Array<int> d(c,int(3));
-    int e[]={2,3,4};
-    Array<int> f(e,3);
-    Array<int> g=d+f;
-    for(int i=0 ;i<d.size();i++)
-        cout<<g[i]<<" ";
-    cout<<endl;
-    Array<int> h;
-    h=g*5 + 2*g;
-    for(int i=0 ;i<d.size();i++)
-        cout<<h[i]<<" ";
-    cout<<endl;
+            case 2:{
+                int n;
+                cout<<"Enter the size of array: ";
+                cin>>n;
+                cin.ignore();
+                cout<<"Enter the elements: ";
+                int ar[n];
+                for(int i=0;i<n;i++)
+                    cin>>ar[i];
+                cin.ignore();
+                Array<int> a1(ar,n);
+                Array<int> a2(a1);
+                cout<<endl<<"New assigned array: ";
+                for(int i=0;i<a2.size();i++)
+                    cout<<a2[i]<<" ";
+                cout<<endl;
+                getchar();
+                break;
+            }
+
+            case 3:{
+                int n;
+                cout<<"Enter the size of array: ";
+                cin>>n;
+                cin.ignore();
+                cout<<"Enter the elements: ";
+                int ar[n];
+                for(int i=0;i<n;i++)
+                    cin>>ar[i];
+                cin.ignore();
+                Array<int> a(ar,n);
+                cout<<endl;
+                for(int i=0;i<a.size();i++)
+                    cout<<"element "<<i+1<<" : "<<a[i]<<" "<<endl;
+                getchar();
+                break;
+            }
+
+            case 4:{
+                int n;
+                cout<<"Enter the size of array: ";
+                cin>>n;
+                cin.ignore();
+                cout<<"Enter the elements: ";
+                int ar[n];
+                for(int i=0;i<n;i++)
+                    cin>>ar[i];
+                cin.ignore();
+                cout<<"Enter the multiplier: ";
+                int c;
+                cin>>c;
+                cin.ignore();
+                Array<int> a(ar,n);
+                Array<int> a1=a*c;
+                Array<int> a2=c*a;
+                cout<<endl<<"Array*"<<c<<": ";
+                for(int i=0;i<a1.size();i++)
+                    cout<<a1[i]<<" ";
+                cout<<endl;
+                cout<<endl<<c<<"*Array"<<": ";
+                for(int i=0;i<a2.size();i++)
+                    cout<<a2[i]<<" ";
+                cout<<endl;
+                getchar();
+                break;
+            }
+
+            case 5:{
+                return 0;
+            }
+            default:
+                cout<<"Invalid entry"<<endl;
+                getchar();
+                break;
+        }
+    }
     return 0;
 }
