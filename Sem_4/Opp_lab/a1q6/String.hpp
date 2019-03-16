207ace24575e232e00d8a4b470ecc07d855b19c1
Md Sahil <287mdsahil@gmail.com>
2019-03-16 13:55:14 +0530

    a1q6 complete

---
+++ Sem_4/Opp_lab/a1q6/String.hpp
@@ -0,0 +1,154 @@
+#include <iostream>
+using namespace std;
+namespace StringNamespace
+{
+class String
+{
+    int size;
+    char *ptr;
+
+  public:
+    String(int n = 0)
+    {
+        size = n;
+        if (size > 0)
+            ptr = new char[size];
+        else
+            ptr = NULL;
+    }
+
+    String(const char *s)
+    {
+        int n = 0;
+        for (n = 0; s[n] != '\0'; n++)
+            ;
+        size = n;
+        ptr = new char[size];
+        for (int i = 0; i < size; i++)
+        {
+            ptr[i] = s[i];
+        }
+    }
+
+    String(const String &s)
+    {
+        size = s.size;
+        ptr = new char[size];
+        for (int i = 0; i < size; i++)
+        {
+            ptr[i] = s.ptr[i];
+        }
+    }
+
+    ~String()
+    {
+        delete[] ptr;
+    }
+
+    void operator=(const char *s)
+    {
+        int n = 0;
+        for (n = 0; s[n] != '\0'; n++)
+            ;
+        size = n;
+        ptr = new char[size];
+        for (int i = 0; i < size; i++)
+        {
+            ptr[i] = s[i];
+        }
+    }
+
+    int len()
+    {
+        return size;
+    }
+
+    void show()
+    {
+        for (int i = 0; i < size; i++)
+            cout << ptr[i];
+    }
+
+    static int scmp(String &s1, String &s2)
+    {
+        if (s1.size == 0 && s2.size == 0)
+            return 0;
+        else if (s1.size == 0)
+            return -(s2.ptr[0]);
+        else if (s2.size == 0)
+            return s1.ptr[0];
+        else
+        {
+            int n = (s1.size > s2.size) ? s1.size : s2.size;
+            for (int i = 0; i < n; i++)
+            {
+                if (s1.ptr[i] != s2.ptr[i])
+                {
+                    return s1.ptr[i] - s2.ptr[i];
+                }
+            }
+
+            if (s1.size > s2.size)
+                return s1.ptr[n];
+            else if (s1.size < s2.size)
+                return -(s2.ptr[n]);
+            else
+                return 0;
+        }
+    }
+
+    friend String operator+(String &, String &);
+    friend int operator>(String &, String &);
+    friend int operator>=(String &, String &);
+    friend int operator<(String &, String &);
+    friend int operator<=(String &, String &);
+};
+
+String operator+(String &s1, String &s2)
+{
+    String s;
+    s.size = s1.size + s2.size;
+    s.ptr = new char[s.size];
+    for (int i = 0; i < s1.size; i++)
+        s.ptr[i] = s1.ptr[i];
+    for (int j = 0; j < s2.size; j++)
+        s.ptr[s1.size + j] = s2.ptr[j];
+    return s;
+}
+
+int operator>(String &s1, String &s2)
+{
+    int k = String::scmp(s1, s2);
+    if (k > 0)
+        return 1;
+    else
+        return 0;
+}
+
+int operator>=(String &s1, String &s2)
+{
+    int k = String::scmp(s1, s2);
+    if (k >= 0)
+        return 1;
+    else
+        return 0;
+}
+
+int operator<(String &s1, String &s2)
+{
+    int k = String::scmp(s1, s2);
+    if (k < 0)
+        return 1;
+    else
+        return 0;
+}
+
+int operator<=(String &s1, String &s2)
+{
+    int k = String::scmp(s1, s2);
+    if (k <= 0)
+        return 1;
+    else
+        return 0;
+}
+} // namespace StringNamespace
---
+++ Sem_4/Opp_lab/a1q6/a1q6.cpp
@@ -0,0 +1,148 @@
+#include <iostream>
+#include "String.hpp"
+using namespace std;
+using namespace StringNamespace;
+
+void menu()
+{
+    while (1)
+    {
+        system("clear");
+        cout << "MENU:" << endl;
+        cout << "1.Assign one object with another" << endl;
+        cout << "2.Concatinate two strings" << endl;
+        cout << "3.Compare two strings" << endl;
+        cout << "4.Exit" << endl;
+        cout << "Enter choice: ";
+        int choice;
+        cin >> choice;
+        cin.ignore();
+
+        switch (choice)
+        {
+
+        case 1:
+        {
+            cout << "Enter the string : ";
+            char c[100];
+            cin >> c;
+            cin.ignore();
+            String obj1(c);
+            cout << "First object : ";
+            obj1.show();
+            cout << endl;
+            String obj2 = obj1;
+            cout << "Second object : ";
+            obj2.show();
+            getchar();
+            break;
+        }
+
+        case 2:
+        {
+            cout << "Enter the first string : ";
+            char c1[100];
+            cin >> c1;
+            cin.ignore();
+            cout << "Enter the second string : ";
+            char c2[100];
+            cin >> c2;
+            cin.ignore();
+            String s1(c1), s2(c2);
+            String s3 = s1 + s2;
+            cout << "Concatinated string : ";
+            s3.show();
+            getchar();
+            break;
+        }
+
+        case 3:
+        {
+            char c1[100], c2[100];
+            cout << "Enter the first string : ";
+            cin >> c1;
+            cin.ignore();
+            cout << "Enter the second string : ";
+            cin >> c2;
+            cin.ignore();
+            String s1(c1), s2(c2);
+
+            cout << "\nMenu:" << endl;
+            cout << "1. use < operator" << endl;
+            cout << "2. use <= operator" << endl;
+            cout << "3. use > operator" << endl;
+            cout << "4. use >= operator" << endl;
+            cout << "Enter choice : ";
+            int x;
+            cin >> x;
+            cin.ignore();
+
+            switch (x)
+            {
+
+            case 1:
+            {
+                if (s1 < s2)
+                    cout << "true" << endl;
+                else
+                    cout << "false" << endl;
+                getchar();
+                break;
+            }
+
+            case 2:
+            {
+                if (s1 <= s2)
+                    cout << "true" << endl;
+                else
+                    cout << "false" << endl;
+                getchar();
+                break;
+            }
+
+            case 3:
+            {
+                if (s1 > s2)
+                    cout << "true" << endl;
+                else
+                    cout << "false" << endl;
+                getchar();
+                break;
+            }
+
+            case 4:
+            {
+                if (s1 >= s2)
+                    cout << "true" << endl;
+                else
+                    cout << "false" << endl;
+                getchar();
+                break;
+            }
+
+            default:
+                cout << "Invalid input";
+                getchar();
+                break;
+            }
+            break;
+        }
+
+        case 4:
+        {
+            return;
+        }
+
+        default:
+            cout << "Invalid entry" << endl;
+            getchar();
+            break;
+        }
+    }
+}
+
+int main()
+{
+    menu();
+    return 0;
+}
