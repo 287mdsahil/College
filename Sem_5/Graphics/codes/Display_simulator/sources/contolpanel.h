7f4d39109756d31a0813b44e3f9ac48a8bf07dfb
Md Sahil <287mdsahil@gmail.com>
2019-08-10 18:32:58 +0530

    minor changes

--- Sem_5/Graphics/codes/Display_simulator/sources/contolpanel.h
+++ Sem_5/Graphics/codes/Display_simulator/sources/contolpanel.h
@@ -27,11 +27,13 @@ public:
         pixelsizeLabel = new QLabel("Enter the pixel size");
         pixelsizeSpinBox = new QSpinBox();
         pixelsizeSpinBox->setRange(1, 10);
+        pixelsizeSpinBox->setValue(5);
         pixelsizeLabel->setBuddy(pixelsizeSpinBox);
 
         noOfPixelsLabel = new QLabel("Enter the pixel size");
         noOfPixelsSpinBox = new QSpinBox();
         noOfPixelsSpinBox->setRange(10, 200);
+        noOfPixelsSpinBox->setValue(100);
         noOfPixelsLabel->setBuddy(noOfPixelsSpinBox);
 
         setGraphButton = new QPushButton("Set Graph");
