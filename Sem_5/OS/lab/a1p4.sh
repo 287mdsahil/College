251685e00f3f059c9c62c1088b5121aba7627603
Md Sahil <287mdsahil@gmail.com>
2019-09-03 11:31:09 +0530

    a1p4 matched only whole words

--- Sem_5/OS/lab/a1p4.sh
+++ Sem_5/OS/lab/a1p4.sh
@@ -5,20 +5,20 @@ read -p "Enter file name 2: " file2
 read -p "Enter file name 3: " file3
 read -p "Enter file name 4: " file4
 
-printfCount1=$(grep -c "printf" $file1)
-printfCount2=$(grep -c "printf" $file2)
-printfCount3=$(grep -c "printf" $file3)
-printfCount4=$(grep -c "printf" $file4)
+printfCount1=$(grep -wc "printf" $file1)
+printfCount2=$(grep -wc "printf" $file2)
+printfCount3=$(grep -wc "printf" $file3)
+printfCount4=$(grep -wc "printf" $file4)
 
-scanfCount1=$(grep -c "scanf" $file1)
-scanfCount2=$(grep -c "scanf" $file2)
-scanfCount3=$(grep -c "scanf" $file3)
-scanfCount4=$(grep -c "scanf" $file4)
+scanfCount1=$(grep -wc "scanf" $file1)
+scanfCount2=$(grep -wc "scanf" $file2)
+scanfCount3=$(grep -wc "scanf" $file3)
+scanfCount4=$(grep -wc "scanf" $file4)
 
-intCount1=$(grep -c "int" $file1)
-intCount2=$(grep -c "int" $file2)
-intCount3=$(grep -c "int" $file3)
-intCount4=$(grep -c "int" $file4)
+intCount1=$(grep -wc "int" $file1)
+intCount2=$(grep -wc "int" $file2)
+intCount3=$(grep -wc "int" $file3)
+intCount4=$(grep -wc "int" $file4)
 
 echo -e "File \t\t printf \t scanf \t int"
 echo -e "$file1 \t $printfCount1 \t\t $scanfCount1 \t $intCount1"
