fcd901f7eacd258c4686417f625ae56a0b8d0e5e
Md Sahil <287mdsahil@gmail.com>
2019-09-09 12:41:11 +0530

    a1p7 complete

--- Sem_5/System_programming/prog/a1p7.asm
+++ Sem_5/System_programming/prog/a1p7.asm
@@ -5,7 +5,7 @@
 num1 DB ?
 num2 DB ?
 num DB ?
-inputPrompt DB 13,10,"Enter number: $"
+inputPrompt DB 13,"Enter number: $"
 greaterPrompt DB " is greater than $"
 lesserPrompt DB " is lesser than $"
 equalPrompt DB " is equal to $"
@@ -38,18 +38,39 @@ main proc
     mov ch,num1
     mov cl,num2
     cmp ch,cl
+    jl lesser
+    je equal
     jg greater
-    ;je equal
-    ;jl lesser
 
     greater:
-    mov num,cl
+    mov num,ch
     call outputNum
     lea dx,greaterPrompt
     mov ah, 9
     int 21h
+    mov num,cl
+    call outputNum
+    ret
+
+    equal:
     mov num,ch
     call outputNum
+    lea dx,equalPrompt
+    mov ah, 9
+    int 21h
+    mov num,cl
+    call outputNum
+    ret
+
+    lesser:
+    mov num,ch
+    call outputNum
+    lea dx,lesserPrompt
+    mov ah, 9
+    int 21h
+    mov num,cl
+    call outputNum
+    ret
 
 
 
@@ -64,22 +85,24 @@ getNum proc
 
     mov dl, 00
     mov cl, 04
-    shl dl, cl
-
+    
     getNumber:
+
     call getChar
     cmp al, 13
     je inputDone
     cmp al, 10
     je inputDone
 
-    sub al, 48;
+    shl dl,cl
+    sub al, 48
     cmp al, 9
     jle isNumber
     sub al, 7
 
     isNumber:
-    add dl, al
+    or dl, al
+    
     jmp getNumber
 
     inputDone:
@@ -105,22 +128,22 @@ outputNum proc
     mov cl, 4
     mov dl, num
     shr dl,cl
-    and dl, 15
-    add dl,48
-    cmp dl,58
+    and dl, 0fh
+    cmp dl,0ah
     jl isNumber2
     add dl,7
     isNumber2:
+    add dl,48
     mov ah, 2
     int 21h
 
     mov dl, num
-    and dl, 15
-    add dl, 48
-    cmp dl, 58
+    and dl, 0fh
+    cmp dl, 0ah
     jl isNumber1
-    add dl, 7
+    add dl, 07h
     isNumber1:
+    add dl,48
     mov ah, 2
     int 21h
 
