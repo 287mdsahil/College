2ccbbc84f5bc5e272af57071dfc54f9e9db9d23b
Md Sahil <287mdsahil@gmail.com>
2020-01-23 18:29:38 +0530

    added dictionary

--- Sem_6/compiler_design/lab/a1/lexicalAnalyzer/Dictionary.java
+++ Sem_6/compiler_design/lab/a1/lexicalAnalyzer/Dictionary.java
@@ -14,27 +14,64 @@ public class Dictionary {
 
 	private void  generate() {
 		
-		// int 0 for artiles
-		File file = new File("lexicalAnalyzer/Dict/articles");
+		// int 0 for adjectives
+		File file = new File("lexicalAnalyzer/Dict/adjectives");
 		try {
 			Scanner fout = new Scanner(file);
 			while(fout.hasNext()) {
 				dict.put(fout.next(),0);
 			}
 		} catch (FileNotFoundException e) {
-			System.out.println("articles file not found");
+			System.out.println("adjectives file not found");
 			System.exit(0);
 		}
 
-		// int 1 for perpositons
-		file = new File("lexicalAnalyzer/Dict/prepositions");
+		// int 1 for articles
+		file = new File("lexicalAnalyzer/Dict/articles");
 		try {
 			Scanner fout = new Scanner(file);
 			while(fout.hasNext()) {
 				dict.put(fout.next(),1);
 			}
 		} catch (FileNotFoundException e) {
+			System.out.println("articles file not found");
+			System.exit(0);
+		}
+	
+		// int 2 for conjunctions
+		file = new File("lexicalAnalyzer/Dict/conjunctions");
+		try {
+			Scanner fout = new Scanner(file);
+			while(fout.hasNext()) {
+				dict.put(fout.next(),2);
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("conjunctions file not found");
+			System.exit(0);
+		}
+
+		// int 3 for prepositions
+		file = new File("lexicalAnalyzer/Dict/prepositions");
+		try {
+			Scanner fout = new Scanner(file);
+			while(fout.hasNext()) {
+				dict.put(fout.next(),3);
+			}
+		} catch (FileNotFoundException e) {
 			System.out.println("prepositions file not found");
+			System.exit(0);
+		}
+
+		
+		// int 4 for verbs
+		file = new File("lexicalAnalyzer/Dict/verbs");
+		try {
+			Scanner fout = new Scanner(file);
+			while(fout.hasNext()) {
+				dict.put(fout.next(),4);
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("verbs file not found");
 			System.exit(0);
 		}
 	}
