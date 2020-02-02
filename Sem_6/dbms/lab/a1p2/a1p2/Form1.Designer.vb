5e7a0fadba8e0f6ddf69599f51f7991d3e8f8276
Md Sahil <287mdsahil@gmail.com>
2020-02-03 02:35:05 +0530

    Created add method in a1p2

--- Sem_6/dbms/lab/a1p2/a1p2/Form1.Designer.vb
+++ Sem_6/dbms/lab/a1p2/a1p2/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,7 +20,7 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.TabControl1 = New System.Windows.Forms.TabControl()
         Me.TabPage1 = New System.Windows.Forms.TabPage()
@@ -38,18 +38,39 @@ Partial Class Form1
         Me.Label1 = New System.Windows.Forms.Label()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader2 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader3 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader4 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader5 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader6 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.TabPage3 = New System.Windows.Forms.TabPage()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.TextBox7 = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.TextBox8 = New System.Windows.Forms.TextBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.TextBox9 = New System.Windows.Forms.TextBox()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
         Me.TabControl1.SuspendLayout()
         Me.TabPage1.SuspendLayout()
+        Me.TabPage2.SuspendLayout()
+        Me.TabPage3.SuspendLayout()
         Me.SuspendLayout()
         '
         'TabControl1
         '
         Me.TabControl1.Controls.Add(Me.TabPage1)
         Me.TabControl1.Controls.Add(Me.TabPage2)
-        Me.TabControl1.Location = New System.Drawing.Point(10, 12)
+        Me.TabControl1.Controls.Add(Me.TabPage3)
+        Me.TabControl1.Location = New System.Drawing.Point(13, 15)
+        Me.TabControl1.Margin = New System.Windows.Forms.Padding(4)
         Me.TabControl1.Name = "TabControl1"
         Me.TabControl1.SelectedIndex = 0
-        Me.TabControl1.Size = New System.Drawing.Size(354, 246)
+        Me.TabControl1.Size = New System.Drawing.Size(472, 303)
         Me.TabControl1.TabIndex = 13
         '
         'TabPage1
@@ -67,10 +88,11 @@ Partial Class Form1
         Me.TabPage1.Controls.Add(Me.Button1)
         Me.TabPage1.Controls.Add(Me.Label1)
         Me.TabPage1.Controls.Add(Me.TextBox1)
-        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 25)
+        Me.TabPage1.Margin = New System.Windows.Forms.Padding(4)
         Me.TabPage1.Name = "TabPage1"
-        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage1.Size = New System.Drawing.Size(346, 220)
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(4)
+        Me.TabPage1.Size = New System.Drawing.Size(464, 274)
         Me.TabPage1.TabIndex = 0
         Me.TabPage1.Text = "Search"
         Me.TabPage1.UseVisualStyleBackColor = True
@@ -78,88 +100,99 @@ Partial Class Form1
         'Label6
         '
         Me.Label6.AutoSize = True
-        Me.Label6.Location = New System.Drawing.Point(112, 161)
+        Me.Label6.Location = New System.Drawing.Point(149, 198)
+        Me.Label6.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(38, 13)
+        Me.Label6.Size = New System.Drawing.Size(49, 17)
         Me.Label6.TabIndex = 25
         Me.Label6.Text = "Phone"
         '
         'TextBox6
         '
-        Me.TextBox6.Location = New System.Drawing.Point(163, 161)
+        Me.TextBox6.Location = New System.Drawing.Point(217, 198)
+        Me.TextBox6.Margin = New System.Windows.Forms.Padding(4)
         Me.TextBox6.Name = "TextBox6"
-        Me.TextBox6.Size = New System.Drawing.Size(138, 20)
+        Me.TextBox6.Size = New System.Drawing.Size(183, 22)
         Me.TextBox6.TabIndex = 24
         '
         'Label5
         '
         Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(112, 135)
+        Me.Label5.Location = New System.Drawing.Point(149, 166)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(45, 13)
+        Me.Label5.Size = New System.Drawing.Size(60, 17)
         Me.Label5.TabIndex = 23
         Me.Label5.Text = "Address"
         '
         'TextBox5
         '
-        Me.TextBox5.Location = New System.Drawing.Point(163, 135)
+        Me.TextBox5.Location = New System.Drawing.Point(217, 166)
+        Me.TextBox5.Margin = New System.Windows.Forms.Padding(4)
         Me.TextBox5.Name = "TextBox5"
-        Me.TextBox5.Size = New System.Drawing.Size(138, 20)
+        Me.TextBox5.Size = New System.Drawing.Size(183, 22)
         Me.TextBox5.TabIndex = 22
         '
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(122, 112)
+        Me.Label4.Location = New System.Drawing.Point(163, 138)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(35, 13)
+        Me.Label4.Size = New System.Drawing.Size(45, 17)
         Me.Label4.TabIndex = 21
         Me.Label4.Text = "Name"
         '
         'TextBox4
         '
-        Me.TextBox4.Location = New System.Drawing.Point(163, 109)
+        Me.TextBox4.Location = New System.Drawing.Point(217, 134)
+        Me.TextBox4.Margin = New System.Windows.Forms.Padding(4)
         Me.TextBox4.Name = "TextBox4"
-        Me.TextBox4.Size = New System.Drawing.Size(138, 20)
+        Me.TextBox4.Size = New System.Drawing.Size(183, 22)
         Me.TextBox4.TabIndex = 20
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(125, 86)
+        Me.Label3.Location = New System.Drawing.Point(167, 106)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(32, 13)
+        Me.Label3.Size = New System.Drawing.Size(41, 17)
         Me.Label3.TabIndex = 19
         Me.Label3.Text = "Code"
         '
         'TextBox3
         '
-        Me.TextBox3.Location = New System.Drawing.Point(163, 83)
+        Me.TextBox3.Location = New System.Drawing.Point(217, 102)
+        Me.TextBox3.Margin = New System.Windows.Forms.Padding(4)
         Me.TextBox3.Name = "TextBox3"
-        Me.TextBox3.Size = New System.Drawing.Size(138, 20)
+        Me.TextBox3.Size = New System.Drawing.Size(183, 22)
         Me.TextBox3.TabIndex = 18
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(127, 60)
+        Me.Label2.Location = New System.Drawing.Point(169, 74)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(30, 13)
+        Me.Label2.Size = New System.Drawing.Size(38, 17)
         Me.Label2.TabIndex = 17
         Me.Label2.Text = "Dept"
         '
         'TextBox2
         '
-        Me.TextBox2.Location = New System.Drawing.Point(163, 57)
+        Me.TextBox2.Location = New System.Drawing.Point(217, 70)
+        Me.TextBox2.Margin = New System.Windows.Forms.Padding(4)
         Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(138, 20)
+        Me.TextBox2.Size = New System.Drawing.Size(183, 22)
         Me.TextBox2.TabIndex = 16
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(38, 31)
+        Me.Button1.Location = New System.Drawing.Point(51, 38)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(88, 19)
+        Me.Button1.Size = New System.Drawing.Size(117, 23)
         Me.Button1.TabIndex = 15
         Me.Button1.Text = "Search"
         Me.Button1.UseVisualStyleBackColor = True
@@ -167,40 +200,185 @@ Partial Class Form1
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(132, 34)
+        Me.Label1.Location = New System.Drawing.Point(176, 42)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(25, 13)
+        Me.Label1.Size = New System.Drawing.Size(32, 17)
         Me.Label1.TabIndex = 14
         Me.Label1.Text = "Roll"
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(163, 31)
+        Me.TextBox1.Location = New System.Drawing.Point(217, 38)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(4)
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(138, 20)
+        Me.TextBox1.Size = New System.Drawing.Size(183, 22)
         Me.TextBox1.TabIndex = 13
         '
         'TabPage2
         '
-        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage2.Controls.Add(Me.ListView1)
+        Me.TabPage2.Location = New System.Drawing.Point(4, 25)
+        Me.TabPage2.Margin = New System.Windows.Forms.Padding(4)
         Me.TabPage2.Name = "TabPage2"
-        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage2.Size = New System.Drawing.Size(346, 220)
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(4)
+        Me.TabPage2.Size = New System.Drawing.Size(464, 274)
         Me.TabPage2.TabIndex = 1
         Me.TabPage2.Text = "Display"
         Me.TabPage2.UseVisualStyleBackColor = True
         '
+        'ListView1
+        '
+        Me.ListView1.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1, Me.ColumnHeader2, Me.ColumnHeader3, Me.ColumnHeader4, Me.ColumnHeader5, Me.ColumnHeader6})
+        Me.ListView1.Location = New System.Drawing.Point(4, 3)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(459, 274)
+        Me.ListView1.TabIndex = 0
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        Me.ListView1.View = System.Windows.Forms.View.Details
+        '
+        'ColumnHeader1
+        '
+        Me.ColumnHeader1.Text = "Roll"
+        '
+        'ColumnHeader2
+        '
+        Me.ColumnHeader2.Text = "Dept"
+        '
+        'ColumnHeader3
+        '
+        Me.ColumnHeader3.Text = "Code"
+        '
+        'ColumnHeader4
+        '
+        Me.ColumnHeader4.Text = "Name"
+        '
+        'ColumnHeader5
+        '
+        Me.ColumnHeader5.Text = "Address"
+        Me.ColumnHeader5.Width = 82
+        '
+        'ColumnHeader6
+        '
+        Me.ColumnHeader6.Text = "Phone"
+        Me.ColumnHeader6.Width = 122
+        '
+        'TabPage3
+        '
+        Me.TabPage3.Controls.Add(Me.ComboBox1)
+        Me.TabPage3.Controls.Add(Me.Label7)
+        Me.TabPage3.Controls.Add(Me.TextBox7)
+        Me.TabPage3.Controls.Add(Me.Label8)
+        Me.TabPage3.Controls.Add(Me.TextBox8)
+        Me.TabPage3.Controls.Add(Me.Label9)
+        Me.TabPage3.Controls.Add(Me.TextBox9)
+        Me.TabPage3.Controls.Add(Me.Label11)
+        Me.TabPage3.Controls.Add(Me.Button2)
+        Me.TabPage3.Location = New System.Drawing.Point(4, 25)
+        Me.TabPage3.Name = "TabPage3"
+        Me.TabPage3.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage3.Size = New System.Drawing.Size(464, 274)
+        Me.TabPage3.TabIndex = 2
+        Me.TabPage3.Text = "Add"
+        Me.TabPage3.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(84, 28)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(184, 24)
+        Me.ComboBox1.TabIndex = 39
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(17, 124)
+        Me.Label7.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(49, 17)
+        Me.Label7.TabIndex = 38
+        Me.Label7.Text = "Phone"
+        '
+        'TextBox7
+        '
+        Me.TextBox7.Location = New System.Drawing.Point(85, 124)
+        Me.TextBox7.Margin = New System.Windows.Forms.Padding(4)
+        Me.TextBox7.Name = "TextBox7"
+        Me.TextBox7.Size = New System.Drawing.Size(183, 22)
+        Me.TextBox7.TabIndex = 37
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(17, 92)
+        Me.Label8.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(60, 17)
+        Me.Label8.TabIndex = 36
+        Me.Label8.Text = "Address"
+        '
+        'TextBox8
+        '
+        Me.TextBox8.Location = New System.Drawing.Point(85, 92)
+        Me.TextBox8.Margin = New System.Windows.Forms.Padding(4)
+        Me.TextBox8.Name = "TextBox8"
+        Me.TextBox8.Size = New System.Drawing.Size(183, 22)
+        Me.TextBox8.TabIndex = 35
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(17, 63)
+        Me.Label9.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(45, 17)
+        Me.Label9.TabIndex = 34
+        Me.Label9.Text = "Name"
+        '
+        'TextBox9
+        '
+        Me.TextBox9.Location = New System.Drawing.Point(85, 60)
+        Me.TextBox9.Margin = New System.Windows.Forms.Padding(4)
+        Me.TextBox9.Name = "TextBox9"
+        Me.TextBox9.Size = New System.Drawing.Size(183, 22)
+        Me.TextBox9.TabIndex = 33
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(17, 35)
+        Me.Label11.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(41, 17)
+        Me.Label11.TabIndex = 30
+        Me.Label11.Text = "Code"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(302, 19)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(4)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(141, 61)
+        Me.Button2.TabIndex = 28
+        Me.Button2.Text = "Add"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(376, 270)
+        Me.ClientSize = New System.Drawing.Size(501, 332)
         Me.Controls.Add(Me.TabControl1)
+        Me.Margin = New System.Windows.Forms.Padding(4)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.TabControl1.ResumeLayout(False)
         Me.TabPage1.ResumeLayout(False)
         Me.TabPage1.PerformLayout()
+        Me.TabPage2.ResumeLayout(False)
+        Me.TabPage3.ResumeLayout(False)
+        Me.TabPage3.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
@@ -220,5 +398,21 @@ Partial Class Form1
     Friend WithEvents Label1 As System.Windows.Forms.Label
     Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
     Friend WithEvents TabPage2 As System.Windows.Forms.TabPage
-
+    Friend WithEvents ListView1 As ListView
+    Friend WithEvents ColumnHeader1 As ColumnHeader
+    Friend WithEvents ColumnHeader2 As ColumnHeader
+    Friend WithEvents ColumnHeader3 As ColumnHeader
+    Friend WithEvents ColumnHeader4 As ColumnHeader
+    Friend WithEvents ColumnHeader5 As ColumnHeader
+    Friend WithEvents ColumnHeader6 As ColumnHeader
+    Friend WithEvents TabPage3 As TabPage
+    Friend WithEvents Label7 As Label
+    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents TextBox8 As TextBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents TextBox9 As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label11 As Label
 End Class
--- Sem_6/dbms/lab/a1p2/a1p2/Form1.vb
+++ Sem_6/dbms/lab/a1p2/a1p2/Form1.vb
@@ -4,10 +4,25 @@
     Dim dcode = New String() {"CSE", "ETCE", "MECH"}
     Dim dname = New String() {"Computer Science", "Electronics", "Mechanical"}
 
+    Private Sub DisplayRefresh()
+        ListView1.Items.Clear()
+        For i = 0 To UBound(students)
+            Dim Item1 As New ListViewItem(students(i).roll)
+            Item1.SubItems.Add(students(i).dept)
+            Item1.SubItems.Add(students(i).code)
+            Item1.SubItems.Add(students(i).name)
+            Item1.SubItems.Add(students(i).address)
+            Item1.SubItems.Add(students(i).phone)
+            ListView1.Items.Add(Item1)
+        Next
+    End Sub
+
     Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         students(0) = New Student(1, dname(0), dcode(0), "Sahil", "Dubrajpur", "12345678")
         students(1) = New Student(2, dname(1), dcode(1), "Boote", "Durgapur", "14464524")
         students(2) = New Student(3, dname(2), dcode(2), "Tommy", "Baruipur", "52454525")
+
+        'Tab1
 
         TextBox2.Visible = False
         Label2.Visible = False
@@ -19,6 +34,16 @@
         Label5.Visible = False
         TextBox6.Visible = False
         Label6.Visible = False
+
+        'Tab 2
+        ListView1.View = View.Details
+        ListView1.LabelEdit = False
+        ListView1.FullRowSelect = True
+        ListView1.GridLines = True
+        DisplayRefresh()
+
+        'Tab 3
+        ComboBox1.Items.AddRange(dcode)
 
     End Sub
 
@@ -48,4 +73,22 @@
         Next
     End Sub
 
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim d As Int32 = ComboBox1.SelectedIndex
+        Dim sdcode As String = Me.dcode(d)
+        Dim sdname As String = Me.dname(d)
+        Dim sname As String = TextBox9.Text
+        Dim saddress As String = TextBox8.Text
+        Dim sphone As String = TextBox7.Text
+        Dim sroll As Int32 = UBound(students) + 1
+
+        students(UBound(students)) = New Student(sroll, sdname, sdcode, sname, saddress, sphone)
+        DisplayRefresh()
+        MessageBox.Show("added with Roll:")
+        TextBox9.Clear()
+        TextBox7.Clear()
+        TextBox8.Clear()
+        ComboBox1.SelectedIndex = -1
+
+    End Sub
 End Class
