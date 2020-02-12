7108d8fcbb172cd46aaaf4f732b09c6e35b0b513
Md Sahil <287mdsahil@gmail.com>
2020-02-12 23:03:22 +0530

    split input lines

--- Sem_6/compiler_design/lab/a2/lexicalAnalyzer/LexicalAnalyzer.java
+++ Sem_6/compiler_design/lab/a2/lexicalAnalyzer/LexicalAnalyzer.java
@@ -7,7 +7,42 @@ public class LexicalAnalyzer {
 
 	private static ArrayList<String> lines = new ArrayList<String>();
 	// An array of strings to hold lines in the input file
+	
 
+	private static String getDelimitersRegex() {
+		// Function to read the input file
+		File file = new File("lexicalAnalyzer/Dict/delimiters");
+		String delimiters = "( )|(\t)|(\n)|(\r)";
+		try {
+			Scanner fin = new Scanner(file);
+			while(fin.hasNext()) {
+				//System.out.println(fin.nextLine());
+				String s = fin.nextLine();
+				delimiters += "|((?<=" + s + ")";
+				delimiters += "|(?=" + s + "))";
+				
+			}
+			fin.close();
+		} catch(FileNotFoundException e) {
+			System.out.println("Error:" + e.getMessage());
+			e.printStackTrace();
+			System.exit(0);
+		}
+
+		return delimiters;
+	}
+
+	private static void tokenize() {
+		for(String line : lines) {
+			String wordarr[] = line.split(getDelimitersRegex());
+			List<String> words = new ArrayList<String>(
+					Arrays.asList(wordarr));
+			words.removeAll(Arrays.asList("",null));
+			for(String word : words) {
+				System.out.println(word);
+			}
+		}
+	}
 
 	private static void readFile(String filename) {
 		// Function to read the input file
@@ -21,11 +56,13 @@ public class LexicalAnalyzer {
 		} catch(FileNotFoundException e) {
 			System.out.println("Error:" + e.getMessage());
 			e.printStackTrace();
+			System.exit(0);
 		}	
 	}
 
 	public static void run(String filename) {
 		System.out.println("lexical analyzer");
-		readFile(filename);	
+		readFile(filename);
+		tokenize();	
 	}
 }
---
+++ Sem_6/compiler_design/lab/a2/test.c
@@ -0,0 +1,5 @@
+int main(){
+	int x=32;
+	int l[] = {x*2,312123}; 
+	return 0;
+}
